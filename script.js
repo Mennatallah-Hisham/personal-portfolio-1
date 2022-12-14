45bf2e15e9df1c9dd2732aa7335aea2ee2c6e32a
unknown <mennahisham1d@gmail.com>
2022-12-14 16:50:49 +0200

    fix

--- script.js
+++ script.js
@@ -19,10 +19,48 @@ if(e.target.classList.contains("main-nav-link")){
 });
 
 
-
-BtnNav.addEventListener("click",()=>{
+const toggleNav =()=>{
     IconClose.classList.toggle("hide");
     IconOpen.classList.toggle("hide");
     header.classList.toggle("open-nav");
-    html.classList.toggle("hideOverflowY");
+    html.classList.toggle("hideOverflowY");  
+}
+BtnNav.addEventListener("click",toggleNav)
+
+const allLinks =document.querySelectorAll('a:link');
+
+
+allLinks.forEach( function(link){
+    link.addEventListener('click',(e)=>{
+
+        e.preventDefault();
+        const href=link.getAttribute("href");
+
+        //scroll back to top
+        if(href==="#"){
+            window.scrollTo({
+                top:0,
+                behavior:"smooth",
+
+            })
+        }
+
+        //scroll to other links
+        if(href !=="#" && href.startsWith("#")){
+          const sectionEl=  document.querySelector(href);
+          sectionEl.scrollIntoView(
+            {
+                behavior:"smooth",
+            }
+          )
+        }
+
+        //close mobile nav
+        if(link.classList.contains('main-nav-link')){
+           toggleNav();
+            
+        }
+
+    });
+
 })
