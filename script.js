cafc2721691ad961745c5b61c047bf6d1865abff
unknown <mennahisham1d@gmail.com>
2022-12-14 16:22:06 +0200

    fix

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 const nav =document.querySelector(".main-nav")
 const navLinks =document.querySelectorAll(".main-nav-link");
 
+const BtnNav =document.querySelector(".btn-mobile-nav");
+const IconClose =document.querySelector(".icon-close");
+const IconOpen =document.querySelector(".icon-open");
+const header =document.querySelector(".header");
+const html =document.querySelector("html")
 
 
 nav.addEventListener("click",(e)=>{
@@ -12,3 +17,12 @@ if(e.target.classList.contains("main-nav-link")){
 }
 
 });
+
+
+
+BtnNav.addEventListener("click",()=>{
+    IconClose.classList.toggle("hide");
+    IconOpen.classList.toggle("hide");
+    header.classList.toggle("open-nav");
+    html.classList.toggle("hideOverflowY");
+})
